9ecd9cd414199106eab2b9c9f839fc13e0743613
Song <quanpengsong@gmail.com>
2020-05-12 13:28:30 +0800

    :hammer: refactor query function

--- query.py
+++ query.py
@@ -1,12 +1,14 @@
 import glob
 import time
-from get_mail_content import parse
+from util import parse
 
 # priority: subject > form, to, date > content > attachments' filename > attachments' content
-weights = [20, 10, 5, 6, 1]
+attributes = ['subject', 'from', 'to', 'date', 'content', 'name', 'content']
+weights = [20, 10, 10, 10, 5, 6, 1]
+assert len(attributes) == len(weights)
 
 
-def sort_by_score(mails: list, scores: list, limit: int) -> list:
+def sort_by_score(scores: list, limit: int) -> list:
     indexes = sorted(range(len(scores)), key=lambda i: scores[i], reverse=True)[:limit]
     result = []
     for index in indexes:
@@ -15,43 +17,37 @@ def sort_by_score(mails: list, scores: list, limit: int) -> list:
     return result
 
 
-def query(keyword: str, mails: list, limit=-1) -> list:
+def query(keyword: str, mails: list, limit=-1, option=None) -> list:
     """
     Keyword query
     :param keyword: as name
     :param mails: we will query this givens mails list
-    :param limit: how many result do you want, -1 means no limit, default is -1
+    :param limit: how many results do you want, -1 means no limit, default is -1
+    :param option: a seven items boolean list, indicates whether the corresponding attributes should be considered
     :return: an index list
     """
     limit = len(mails) if limit is -1 else limit
+    option = [True for _ in range(len(weights))] if option is None else option
+    assert len(option) == len(attributes)
     scores = [0 for x in range(len(mails))]
-    for i in range(len(mails)):
-        mail = mails[i]
-        count = mail["subject"].count(keyword)
-        scores[i] += count * weights[0]
-    for i in range(len(mails)):
-        mail = mails[i]
-        count = mail["from"].count(keyword)
-        count += mail["to"].count(keyword)
-        count += mail["date"].count(keyword)
-        scores[i] += count * weights[1]
-    for i in range(len(mails)):
-        mail = mails[i]
-        count = mail["content"].count(keyword)
-        scores[i] += count * weights[2]
-    for i in range(len(mails)):
-        mail = mails[i]
-        count = 0
-        for attachment in mail["attachments"]:
-            count = attachment["name"].count(keyword)
-        scores[i] += count * weights[3]
-    for i in range(len(mails)):
-        mail = mails[i]
-        count = 0
-        for attachment in mail["attachments"]:
-            count = attachment["content"].count(keyword)
-        scores[i] += count * weights[4]
-    return sort_by_score(mails, scores, limit)
+    for index in range(len(attributes)):
+        attribute = attributes[index]
+        enable = option[index]
+        if not enable:
+            continue
+        if index < 5:
+            for i in range(len(mails)):
+                mail = mails[i]
+                count = mail[attribute].count(keyword)
+                scores[i] += count * weights[index]
+        else:
+            for i in range(len(mails)):
+                mail = mails[i]
+                count = 0
+                for attachment in mail["attachments"]:
+                    count = attachment[attribute].count(keyword)
+                scores[i] += count * weights[index]
+    return sort_by_score(scores, limit)
 
 
 def main():
@@ -63,7 +59,7 @@ def main():
     start = time.time()
     result = query(keyword, mails)
     end = time.time()
-    print(f"{len(result)} results ({end - start} seconds)")
+    print(f"{len(result)} results ({round(end - start, 2)} seconds)")
     for i in result:
         print(mails[i]["subject"])
     print()
