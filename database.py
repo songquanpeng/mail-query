13a3c9aa90ca7a48c112fa2d7e8c99d94acbf761
Song <quanpengsong@gmail.com>
2020-05-16 00:08:11 +0800

    :new: now you can query the database

--- database.py
+++ database.py
@@ -1,14 +1,5 @@
 import sqlite3
-import json
 import time
-
-create_attachments_table = """
-CREATE TABLE IF NOT EXISTS "attachments" (
-"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-"name"	TEXT,
-"content"	TEXT
-);
-"""
 
 create_mails_table = """
 CREATE TABLE IF NOT EXISTS "mails"(
@@ -19,8 +10,8 @@ CREATE TABLE IF NOT EXISTS "mails"(
 "receiver"	TEXT,
 "date"	TEXT,
 "content"	TEXT,
-"attachment"	INTEGER,
-FOREIGN KEY (attachment) REFERENCES attachments (id)
+"attachment_name"	TEXT,
+"attachment_content"	TEXT
 );
 """
 connection = None
@@ -33,7 +24,6 @@ def init(database_path='./data.db'):
     global cursor
     connection = sqlite3.connect(database_path)
     cursor = connection.cursor()
-    cursor.execute(create_attachments_table)
     cursor.execute(create_mails_table)
     return cursor
 
@@ -48,35 +38,37 @@ def close():
 
 def insert(mail: dict):
     global cursor
-    attachment_ids = []
+    attachment_name = ""
+    attachment_content = ""
     for attachment in mail["attachments"]:
-        cursor.execute(
-            "insert into attachments (name, content) values (?, ?)", (attachment['name'], attachment['content']))
-        attachment_ids.append(cursor.lastrowid)
+        attachment_name += attachment['name'] + "\n"
+        attachment_content += attachment['content'] + "\n"
     try:
         cursor.execute(
-            "insert into mails (subject, sender, receiver, 'date', content, attachment, path) values (?,?,?,?,?,?,?)",
-            (mail['subject'], mail['from'],
-             mail['to'], mail['date'],
-             mail['content'],
-             json.dumps(attachment_ids), mail['path']))
+            """insert into mails (path, subject, sender, receiver, 'date', content, attachment_name, attachment_content) 
+            values (?,?,?,?,?,?,?,?)""", (mail['path'], mail['subject'], mail['from'], mail['to'], mail['date'],
+                                          mail['content'], attachment_name, attachment_content))
     except sqlite3.IntegrityError as e:
         print(e)
-
-
-def load_mails(option=list) -> list:
-    pass
 
 
 def query(keyword: str, limit=-1, option=None) -> list:
     start = time.time()
     print("Processing query ...", end=" ")
     option = [True] * 7 if option is None else option
-    result = cursor.execute(f'''select path from mails 
-    where "subject" like "%{keyword}%" 
-    or "from" like "%{keyword}%" or "to" like "%{keyword}%" 
-    or "date" like "%{keyword}%" or "content" like "%{keyword}%" ''')
-
+    sql = f'select path from mails where "subject" like "%{keyword}%" '
+    sql += (f'or "from" like "%{keyword}%" ' if option[1] else "")
+    sql += (f'or "to" like "%{keyword}%" ' if option[2] else "")
+    sql += (f'or "date" like "%{keyword}%" ' if option[3] else "")
+    sql += (f'or "content" like "%{keyword}%" ' if option[4] else "")
+    sql += (f'or "attachment_name" like "%{keyword}%" ' if option[5] else "")
+    sql += (f'or "attachment_content" like "%{keyword}%" ' if option[6] else "")
+    if limit >= 1:
+        sql += f'limit {limit}'
+    paths = cursor.execute(sql)
+    result = []
+    for path in paths:
+        result.append(path[0])
     end = time.time()
     print(f"Done, time cost: {end - start}")
     return result
@@ -84,6 +76,8 @@ def query(keyword: str, limit=-1, option=None) -> list:
 
 def main():
     init()
+    result = query("com")
+    print(result)
     close()
     pass
 
