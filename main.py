bbf799bce19796819963aabd00560d04e88ddebf
Lee Jiangwei <ljwskrskr@outlook.com>
2020-05-16 15:39:40 +0800

    :construction: migrate QWidget to QMainWindow, add statusbar messsages

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import time
 
 from PyQt5.QtWidgets import QLineEdit, QWidget, QListWidget, QLabel, QPushButton, QCheckBox, \
-    QHBoxLayout, QGridLayout, QFileDialog, QMessageBox, QApplication, QStatusBar
+    QHBoxLayout, QGridLayout, QFileDialog, QMessageBox, QApplication, QStatusBar, QMainWindow
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
 from PyQt5.QtGui import QIntValidator
 
@@ -69,9 +69,11 @@ class App(QWidget):
                               "Attachment filename", "Attachment content"]
         self.options = [True for _ in range(len(self.options_names))]
         self.limit = -1
+        self.timer = time.time()
 
         self.dirPathEdit = QLineEdit()
         self.searchEdit = QLineEdit()
+        self.searchBtn = QPushButton("Search")
         self.listWidget = QListWidget()
         self.statusBar = QStatusBar()
 
@@ -83,6 +85,7 @@ class App(QWidget):
         self.dirPathEdit.returnPressed.connect(self.select_dir)
         self.searchEdit.textChanged.connect(self.on_edit_changed)
         self.searchEdit.returnPressed.connect(self.search)
+        self.searchBtn.clicked.connect(self.search)
         self.listWidget.itemDoubleClicked.connect(self.open_file)
 
         self.init_ui()
@@ -90,14 +93,10 @@ class App(QWidget):
         self.searchEdit.setFocus()
 
     def init_ui(self):
-        self.resize(1200, 900)
-        self.setWindowTitle("hello motherfucker")
-
         dirPathLabel = QLabel("Directory path:")
         dirBrowseBtn = QPushButton("Browse")
 
         searchLabel = QLabel("Keyword:")
-        searchBtn = QPushButton("Search")
 
         scopeLabel = QLabel("Search scope:")
 
@@ -110,7 +109,6 @@ class App(QWidget):
         regexpCheckbox = QCheckBox()
 
         dirBrowseBtn.clicked.connect(self.browse_dir)
-        searchBtn.clicked.connect(self.search)
         limitEdit.textChanged.connect(self.on_edit_changed)
         limitEdit.setText("-1")
 
@@ -142,7 +140,7 @@ class App(QWidget):
         grid.addWidget(dirBrowseBtn, 0, 2, 1, 1)
         grid.addWidget(searchLabel, 1, 0)
         grid.addWidget(self.searchEdit, 1, 1)
-        grid.addWidget(searchBtn, 1, 2)
+        grid.addWidget(self.searchBtn, 1, 2)
         grid.addWidget(scopeLabel, 2, 0)
         grid.addItem(optionsHBox, 2, 1, 1, 2)
         grid.addWidget(otherLabel, 3, 0)
@@ -163,6 +161,7 @@ class App(QWidget):
     def select_dir(self):
         self.load_thread = LoadThread(self.directory)
         self.load_thread.trigger.connect(self.on_load_thread_returned)
+        self.timer = time.time()
         self.load_thread.start()
 
     def open_file(self, item):
@@ -178,6 +177,9 @@ class App(QWidget):
             self.search_thread = SearchThread(self.keyword, self.mails, int(self.limit),
                                               self.options)
             self.search_thread.trigger.connect(self.on_search_thread_returned)
+            self.statusBar.showMessage("Processing query...")
+            self.searchBtn.setEnabled(False)
+            self.timer = time.time()
             self.search_thread.start()
         except ValueError:
             QMessageBox.critical(self, "Error", "Limit number must be valid integer.",
@@ -187,16 +189,26 @@ class App(QWidget):
         if message == "success":
             self.fileList = self.load_thread.fileList
             self.mails = self.load_thread.mails
+            if not len(self.mails):
+                self.statusBar.showMessage("No eml file detected in given directory.")
+            else:
+                used_time = time.time() - self.timer
+                self.statusBar.showMessage("Loaded done in " + str(used_time) + " seconds.")
             self.listWidget.clear()
             self.listWidget.addItems(self.fileList)
         else:
             QMessageBox.critical(self, "Error", message, QMessageBox.Ok, QMessageBox.Ok)
 
     def on_search_thread_returned(self):
+        used_time = time.time() - self.timer
         index = self.search_thread.index
         result = [self.fileList[i] for i in index]
         self.listWidget.clear()
         self.listWidget.addItems(result)
+        self.searchBtn.setText("Search")
+        self.searchBtn.setEnabled(True)
+        self.statusBar.showMessage("Got " + str(len(result)) + " results, query done in "
+                                   + str(used_time) + " seconds.")
 
     def on_edit_changed(self, text):
         name = self.sender().objectName()
@@ -218,8 +230,18 @@ class App(QWidget):
             QCloseEvent.ignore()
 
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.resize(1200, 900)
+        self.setWindowTitle("hello motherfucker")
+
+        self.setCentralWidget(App())
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = App()
+    ex = MainWindow()
     ex.show()
     sys.exit(app.exec_())
